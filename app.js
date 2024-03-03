7ceddf2cc4bcc64461d9f0f975d7d905582f9e9f
Jiahao <lucashuamani256w@gmail.com>
2024-03-02 23:24:14 -0500

    Revamp using express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const host = "localhost";
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "./about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "./contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./404.html"));
+});
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
